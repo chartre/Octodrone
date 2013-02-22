b2ae5f7a125641f76932abf5104cb89eb5c2b721
Carlos Rosales Salido <chartre@gmail.com>
2013-02-22 12:40:48 +0100

    Primera prueba con los calculos trigonometricos
    -Se ha modificado el simulink eliminando una de las funciones
    duplicadas. En la funcion que queda se ha realizado el calculo de los
    angulos con varios if's anidados.
    -Se ha modificado tanto simulacontrol como pintascdata para que dibujen
    los vectores errores desde la posicion y desde la currref de enrique.
    (Algo falla)

--- SimuladorControl/pintascdata.m
+++ SimuladorControl/pintascdata.m
@@ -16,7 +16,7 @@
 
 %% Función pintascdata()
 
-function indicadores=pintascdata(scdata,ruta,radio,scVar,valores)
+function indicadores=pintascdata(scdata,ruta,radio,scVar,vE)
 % indicadores=pintascdata(scdata,ruta,radio)
 % 
 % Dibuja las graficas de un ensayo de control
@@ -133,11 +133,11 @@ for i=1:5:size(scVar.signals(1,1).values(:,1),1)
     plot([scVar.signals(1,1).values(i,1),scVar.signals(1,1).values(i,4)],...
         [scVar.signals(1,2).values(i,1),scVar.signals(1,2).values(i,4)],'k')
     % dibuja vector error sumado a CurrRef
-    plot([scVar.signals(1,3).values(i,1),scVar.signals(1,1).values(i,4)],...
-        [scVar.signals(1,3).values(i,2),scVar.signals(1,2).values(i,4)],'m')
+    plot([scVar.signals(1,3).values(i,1),scVar.signals(1,3).values(i,1)+vE(1,1)],...
+        [scVar.signals(1,3).values(i,2),scVar.signals(1,3).values(i,2)+vE(1,2)],'m')
     % dibuja vector error desde pos hasta trayectoria
-    plot([valores(i,5),(valores(i,5)+valores(i,11))],...
-        [valores(i,6),(valores(i,6)+valores(i,12))],'y')
+    plot([scVar.signals(1,1).values(i,1),scVar.signals(1,1).values(i,1)+vE(1,1)],...
+        [scVar.signals(1,2).values(i,1),scVar.signals(1,2).values(i,1)+vE(1,2)],'y')
 end
 % dibuja CurrRef
 plot(scVar.signals(1,3).values(:,1),scVar.signals(1,3).values(:,2),'c')
