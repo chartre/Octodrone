8030c0b7b35a6bbd389496fca1e946aad4ee80fe
Carlos Rosales Salido <chartre@gmail.com>
2013-03-26 19:53:43 +0100

    En construccion modelo de saturacion dinamica
    - Modificado pintascdata para representar currRef en el grafico
    - Modificado simulaControl para probar otra ruta
    - Modificado comparando_modelos para hacer pruebas con la saturacion
    dinamica en funcion de la velocidad que se desea obtener.
    - Modificado simuladorControlCuatrirrotor para añadir los switches
    necesarios para despues añadir el control dinamico de la saturacion.
    Pero aun no se ha añadido nada mas. Hay que pasarlo desde
    comparando_modelos.

--- SimuladorControl/simulaControl.m
+++ SimuladorControl/simulaControl.m
@@ -16,8 +16,8 @@
 
 
 %% Simulación controlador
-ruta=[12 0;12 5;7 -5;-1 4;-6 -5;-2 0];
-% ruta=[12 0;12 12;0 12;0 0];
+ruta=[12 12;12 12];
+% ruta=[12 0;12 5;7 -5;-1 4;-6 -5;-2 0];
 %ruta=intermedio(ruta,2);% Esta funcion añade puntos intermedios.
 radio=0.1;
 sim('simuladorControlCuatrirrotor')
