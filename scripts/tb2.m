3707004198496756dc6f8d37729bea8a6bf92212
Carlos Rosales Salido <chartre@gmail.com>
2013-04-10 02:06:25 +0200

    Bancos de pruebas funcionando
    Terminado "banco de pruebas uno" y hecho el "dos".
    -Modificado el tiempo de simulacion en el simulink. Ahora se ha puesto
    una variable que se modifica directamente en el codigo.
    -tb1 sin cambios importantes (baile de numeros y tabulaciones)
    -Se ha creado pintascdata_tb2 para poder modificar ciertas cosas y que
    el original esté presente para poder seguir usandolo en otros sitios.
    Basicamente se ha comentado la segunda ventana para que no saliera y se
    han eliminado todos los "figures" para poder crear una sola ventana que
    contenga todos los moviminetos (ésta se crea en tb2).
    -tb2 creado eliminando tambien algunas cosas que aparecian en pantalla
    -Creado rendimiento_gpp_tb2 por el mismo motivo que pintascdata_tb2 pero
    con el objetivo de quitar cosas que salian por consola y poder usar los
    datos como gpp y "zonas"

---
+++ scripts/tb2.m
@@ -0,0 +1,62 @@
+%% Banco de pruebas para probar distintas trayectorias de moviemiento del
+% aparato.
+
+%% Calculo de la trayectoria a seguir
+% Se puede definir la distancia de los tramos y el angulo en el que se
+% va a incrementar cada tramo (siempre haciendo los ejes)
+dis = 10; % distancia a recorrer en cada tramo
+ang = pi/6; % angulos a incrementar entre cada prueba
+np = floor(2*pi/ang); % n de pruebas a realizar
+mov = zeros (np,3); % creacion del array de tramos
+output = zeros (np,4);
+radio=0.1;
+
+%% Calculo de los trayectos de la ruta
+for i=1:np
+    mov(i,1)=((i-1)*ang)*180/pi; % angulo en grados
+    mov(i,2)=dis*cos((i-1)*ang); % movimiento en x
+    mov(i,3)=dis*sin((i-1)*ang); % movimiento en y
+end
+rutaEntera = mov(:,2:3);
+
+%% Calculo de los puntos de la ruta
+% for i=1:(size(mov)-1)
+%     rutaEntera(i+1,1:2) = rutaEntera(i+1,1:2)+rutaEntera(i,1:2);
+% end
+
+%% Simulacion por partes
+ruta = zeros(2,2);
+    figure
+    hold on
+
+for i=1:np
+    ruta = rutaEntera(i,1:2);
+    ruta(2,:) = ruta(1,:); % insertamos dos veces el mismo punto para que no deproblemas el simulink
+    
+    %% Simulacion controlador de cada movimiento
+    simtime = size(ruta,1)*10; % le damos un tiempo de ejecucion de 10 segundos por cada trayecto
+    sim('simuladorControlCuatrirrotor')
+
+    %% Representacion grafica y calculo de indicadores
+    indicadores=pintascdata_tb2(scdata,ruta,radio,scVar,pos,vE,modulo_vE,modulo_vCons,Consigna);
+    
+    %% Evaluacion
+    %Preparamos el vector para evaluar rendimiento
+    J=[indicadores.dmedia, indicadores.dmax, indicadores.tiempoRecorrido];
+    taux=indicadores.tiempoMin;
+    
+    %Preparamos la matriz de preferencias
+    %          [-----AD----](-----D----](-----T----](-----I----](-----AI---](---->
+    PhyMatrix=[0         0.0500      0.1000      0.1500      0.2500      0.500;
+        0         0.1000      0.1500      0.2500      0.5000      1.000;
+        taux         2*taux    2.5*taux      4*taux      5*taux     9*taux];
+    
+    Etiquetas={'Distancia Media','Distancia Maxima','Tiempo de Recorrido';
+        'metros',           'metros',        'segundos'};
+    
+    %Evaluamos
+    [GPP, Porcentajes, Zonas]=Rendimiento_GPP(J,PhyMatrix,Etiquetas);
+    output(i,1) = GPP;
+%     output(i,2:4) = Zonas(1,1:3);
+%     output
+end
