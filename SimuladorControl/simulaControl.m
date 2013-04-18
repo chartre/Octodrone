66c88271a650163724a2b3c8992a404686b9c0c6
Carlos Rosales Salido <chartre@gmail.com>
2013-04-18 08:13:43 +0200

    Intentando arreglar desvio sobre la consigna
    Arreglado el tema de las ventanas (se abrian 7)
    Modificado el simulink para añadir la parte que evita que el cacharro se
    vaya hacia atras.

--- SimuladorControl/simulaControl.m
+++ SimuladorControl/simulaControl.m
@@ -16,10 +16,11 @@
 
 
 %% Simulación controlador
+% ruta=[0 10;0 10;0 10;0 10;0 10;0 10;0 10;0 10;0 10;0 10;];
 % ruta=[0 2;8 0;6 -8;0 -7;-8 -8;-7 0;0 1;8 -1]; % Prueba trayectos predomina 'X'
 % ruta=[1 -6;-5 -7;-13 -6;-12 0]; % Prueba de trayectos básicos
-ruta=[3 2;1 -6;-5 -7;-13 -6;-12 0]; % Prueba de trayectos básicos FAIL!!
-% ruta=[12 0;12 5;7 -5;-1 4;-6 -5;-2 0]; % Trayecto original
+% ruta=[3 2;1 -6;-5 -7;-13 -6;-12 0]; % Prueba de trayectos básicos FAIL!!
+ruta=[12 0;12 5;7 -5;-1 4;-6 -5;-2 0]; % Trayecto original
 % ruta=[12 0;12 12;0 12;0 0]; % Trayecto cuadrado, horiz. y vert.
 %ruta=intermedio(ruta,2);% Esta funcion añade puntos intermedios.
 radio=0.1;
@@ -32,7 +33,7 @@ pdi;
 
 
 %% Representación gráfica y cálculo de indicadores
-indicadores=pintascdata(scdata,ruta,radio,scVar,pos,vE,modulo_vE,modulo_vCons,Consigna);
+indicadores=pintascdata(scdata,ruta,radio,scVar,pos,vE,modulo_vE,modulo_vCons,vCons);
 
 
 %% Evaluación 
