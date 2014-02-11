05c2b87e66dc18be36b379eed07aa7e841996f7a
borJaRal <bbrodal@gmail.com>
2014-02-11 16:37:23 +0100

    Limpieza y pruebas para desempeño en preconcurso
    Limpieza de código, definición de sistema tras ajuste de ejes a
    organización.
    ...puede algún otro...

--- ControlDLL/ControlDLL/ControlDLL.cpp
+++ ControlDLL/ControlDLL/ControlDLL.cpp
@@ -1,20 +1,13 @@
 // DLL Template for TrackDrone Lite
-// Author: Sergio Garc
-// Universidad Politecnica de Valencia 2011
 
 #include "stdafx.h"
 #include "ControlDLL.h"
 #include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
 #define Ts 0.06 // El TrackDroneLite va a llamar a la función "Control" cada 0.06 seg.
-
-// Añadido nuestro (a borrar)
-#include <string>
-#include <fstream>
-#include <iostream>
-#include <time.h>
-
 
 // Estas variables van a perdurar a los ciclos
 double A[2];
@@ -110,16 +103,15 @@ extern "C" {
 		// Preprocessing
 
 		//Roll Parameters
-		position[0]=0; // Asignamos valor 0 a la posición X para probar únicamente el eje Y sobre la posición nula de X. Anular también "action[]" PID del eje.
 		actualWayPoint [0] = wayPointX[i];
 		d_wx = wayPointX[i] - position[0];
 
 		//Pitch Parameters
 		
 		actualWayPoint [1] = wayPointY[i];
-		d_wy = wayPointY[i] + (position[1]); // Cambiamos signos en valor posición para eje Y por entender convenio de signos a la inversa. También se modifica en otro uso de esta variable más adelante del código.
+		d_wy = wayPointY[i] - (position[1]); // Cambiamos signos en valor posición para eje Y por entender convenio de signos a la inversa. También se modifica en otro uso de esta variable más adelante del código.
 		
-		///* Sistema de trayectorias (aka "parte de Enrique")
+		///* Sistema de trayectorias
 		//===================================================
 		//Las entradas son:
 		//-siguiente waypoint (wayPointX/Y[i])
@@ -129,8 +121,8 @@ extern "C" {
 		//Las salidas son:
 		//-CurrRef (ref_x/y)	*/
 
-		int maxVx = 2;
-		int maxVy = 1;
+		int maxVx = 1;
+		int maxVy = 1.5;
 		//DistXY = SPPos-LastRef;
 		double dist_x = wayPointX[i] - ref_x_1;
 		double dist_y = wayPointY[i] - ref_y_1;
@@ -171,7 +163,7 @@ extern "C" {
 			}
 		}
 
-		///* Corrector (aka "nuestra parte")
+		///* Corrector
 		//==================================
 		//Las entradas a esta seccion son:
 		//-posicion anterior (p_x/y_1)
@@ -181,135 +173,163 @@ extern "C" {
 		//La salida es:
 		//-consigna (por-definir)	*/
 
-		//A[0] = wayPointX[i-1];
-		//A[1] = wayPointX[i-1];??????????????????????????????????????????????????????????????????????????????????????????????????????????????????
+		double eps = 0.001;
 
-		//B[0] = wayPointX[i];
-		//B[1] = wayPointY[i];
+		A[0] = wayPointX[i-1];
+		A[1] = wayPointY[i-1];
 
-		////construimos vector posicion
-		//C[0] = position[0];
-		//C[1] = position[1];
+		B[0] = wayPointX[i];
+		B[1] = wayPointY[i];
 
-		//// creamos vector director de la recta AB (recta r)
-		//Vr[0] = B[0]-A[0];
-		//Vr[1] = B[1]-A[1];
+		//construimos vector posicion
+		C[0] = position[0];
+		C[1] = position[1];
 
-		//// iniciamos vector Vs
-		//// (no necesario en c++)
+		// creamos vector director de la recta AB (recta r)
+		Vr[0] = B[0]-A[0];
+		Vr[1] = B[1]-A[1];
 
-		//// creamos vector perpendicular a Vr
-		//Vs[0] = -Vr[1];
-		//Vs[1] = Vr[0];
+		// iniciamos vector Vs
+		// (no necesario en c++)
 
-		//// creamos matriz de vectores directores
-		//MatrA[0][0] = Vr[0];
-		//MatrA[0][1] = -Vs[0];
-		//MatrA[1][0] = Vr[1];
-		//MatrA[1][1] = -Vs[1];
+		// creamos vector perpendicular a Vr
+		Vs[0] = -Vr[1];
+		Vs[1] = Vr[0];
 
-		//// creamos matriz de valores independientes
-		//MatrB[0] = C[0]-A[0];
-		//MatrB[1] = C[1]-A[1];
+		// creamos matriz de vectores directores
+		MatrA[0][0] = Vr[0];
+		MatrA[0][1] = -Vs[0];
+		MatrA[1][0] = Vr[1];
+		MatrA[1][1] = -Vs[1];
 
-		//// calculamos los valores de las K's de cada recta
+		// creamos matriz de valores independientes
+		MatrB[0] = C[0]-A[0];
+		MatrB[1] = C[1]-A[1];
 
-		//// Inversa de MatrA
-		//double detA = (MatrA[0][0]*MatrA[1][1]) - (MatrA[0][1]*MatrA[1][0]);
-		//MatrAt[0][0]=MatrA[1][1];
-		//MatrAt[0][1]=-MatrA[0][1];
-		//MatrAt[1][0]=-MatrA[1][0];
-		//MatrAt[1][1]=MatrA[0][0];
+		// calculamos los valores de las K's de cada recta
 
-		//MatrAt[0][0]=MatrAt[0][0]/detA;
-		//MatrAt[0][1]=MatrAt[0][1]/detA;
-		//MatrAt[1][0]=MatrAt[1][0]/detA;
-		//MatrAt[1][1]=MatrAt[1][1]/detA;
+		// Inversa de MatrA
+		double detA = (MatrA[0][0]*MatrA[1][1]) - (MatrA[0][1]*MatrA[1][0]);
+		MatrAt[0][0]=MatrA[1][1];
+		MatrAt[0][1]=-MatrA[0][1];
+		MatrAt[1][0]=-MatrA[1][0];
+		MatrAt[1][1]=MatrA[0][0];
 
-		//// Matriz de valores k
-		//MatrK[0]=(MatrAt[0][0]*MatrB[0]) + (MatrA[0][1]*MatrB[1]);
-		//MatrK[1]=(MatrAt[1][0]*MatrB[0]) + (MatrA[1][1]*MatrB[1]);
+		if (abs(detA)>eps)
+		{
+		MatrAt[0][0]=MatrAt[0][0]/detA;
+		MatrAt[0][1]=MatrAt[0][1]/detA;
+		MatrAt[1][0]=MatrAt[1][0]/detA;
+		MatrAt[1][1]=MatrAt[1][1]/detA;
+		}
+		else
+		{
+		MatrAt[0][0]=MatrAt[0][0]/eps;
+		MatrAt[0][1]=MatrAt[0][1]/eps;
+		MatrAt[1][0]=MatrAt[1][0]/eps;
+		MatrAt[1][1]=MatrAt[1][1]/eps;
+		}
+		
 
-		//// sacamos el punto D intereseccion de las rectas
-		//// D = A + K(1,1)*Vr;
-		//D[0]=A[0]+MatrK[0]*Vr[0];
-		//D[1]=A[1]+MatrK[0]*Vr[1];
+		// Matriz de valores k
+		MatrK[0]=(MatrAt[0][0]*MatrB[0]) + (MatrA[0][1]*MatrB[1]);
+		MatrK[1]=(MatrAt[1][0]*MatrB[0]) + (MatrA[1][1]*MatrB[1]);
 
-		//vE[0] = D[0]-C[0];
-		//vE[1] = D[1]-C[1];
+		// sacamos el punto D intereseccion de las rectas
+		// D = A + K(1,1)*Vr;
+		D[0]=A[0]+MatrK[0]*Vr[0];
+		D[1]=A[1]+MatrK[0]*Vr[1];
 
-		//// Calculo cuando CurrRef no está sobre la trayectoria
-		//// Se añade esta segunda parte para corregir desviaciones de la trayectoria 
-		//// ideal teórica de la forma apropiada y evitar discrepancias según el método 
-		//// de gestión de consigna empleado. Funciona correctamente.
+		vE[0] = D[0]-C[0];
+		vE[1] = D[1]-C[1];
 
-		//// creamos matriz de valores independientes
-		//MatrC[0] = ref_x - A[0];
-		//MatrC[1] = ref_y - A[1];
+		// Calculo cuando CurrRef no está sobre la trayectoria
+		// Se añade esta segunda parte para corregir desviaciones de la trayectoria 
+		// ideal teórica de la forma apropiada y evitar discrepancias según el método 
+		// de gestión de consigna empleado. Funciona correctamente.
 
-		//// calculamos los valores de las K's de cada recta
-		//// K1 = inv(MatrA)*MatrC;
-		//MatrK1[0]=(MatrAt[0][0]*MatrC[0]) + (MatrAt[0][1]*MatrC[1]);
-		//MatrK1[1]=(MatrAt[1][0]*MatrC[0]) + (MatrAt[1][1]*MatrC[1]);
+		// creamos matriz de valores independientes
+		MatrC[0] = ref_x - A[0];
+		MatrC[1] = ref_y - A[1];
 
-		//// sacamos el punto E, vertical desde CurrRef, intereseccion de las rectas
-		//// con trayectoria
-		//// E = A + K1(1,1)*Vr;
-		//E[0]=A[0]+MatrK1[0]*Vr[0];
-		//E[1]=A[1]+MatrK1[0]*Vr[1];
+		// calculamos los valores de las K's de cada recta
+		// K1 = inv(MatrA)*MatrC;
+		MatrK1[0]=(MatrAt[0][0]*MatrC[0]) + (MatrAt[0][1]*MatrC[1]);
+		MatrK1[1]=(MatrAt[1][0]*MatrC[0]) + (MatrAt[1][1]*MatrC[1]);
 
-		//// Permite seleccionar el instante al que afecta la
-		//// magnitud de la corrección (posible introducción a la corrección variable
-		//// selectiva)
-		//double normvE = sqrt((vE[0]*vE[0]) + (vE[1]*vE[1]));
+		// sacamos el punto E, vertical desde CurrRef, intereseccion de las rectas
+		// con trayectoria
+		// E = A + K1(1,1)*Vr;
+		E[0]=A[0]+MatrK1[0]*Vr[0];
+		E[1]=A[1]+MatrK1[0]*Vr[1];
 
-		//if (normvE > 0.5)
-		//{
-		//	consigna[0] = E[0] + vE[0]*10;
-		//	consigna[1] = E[1] + vE[1]*10;
-		//}
-		//else
-		//{
-		//	consigna[0] = E[0] + vE[0]*3;
-		//	consigna[1] = E[1] + vE[1]*3;
-		//}
-		//// Fin de la parte del corrector//
+		// Permite seleccionar el instante al que afecta la
+		// magnitud de la corrección (posible introducción a la corrección variable
+		// selectiva)
+		double normvE = sqrt((vE[0]*vE[0]) + (vE[1]*vE[1]));
+
+		if (normvE > 0.5)
+		{
+			consigna[0] = E[0] + vE[0]*10;
+			consigna[1] = E[1] + vE[1]*10;
+		}
+		else
+		{
+			consigna[0] = E[0] + vE[0]*3;
+			consigna[1] = E[1] + vE[1]*3;
+		}
+		// Fin de la parte del corrector//
+
+		ref_x=consigna[0];
+		ref_y=consigna[1];
 
 		e_kx = ref_x - position[0];
-		e_ky = ref_y + (position[1]);
+		e_ky = ref_y - (position[1]);
 
 		// Acciones de control
 		// Control PID Law for X-PITCH
 		// Pitch Param¡eters
-		Kcx = 0.1;
+		/*Kcx = 0.1;
 		Tix = 0;
 		Tdx = 0.05;
-		int Nx = 12;
-		action[0] = 0; // -(Kcx*e_kx + ((Tdx*Nx)/(1+(Nx*Ts/(e_kx_1 - e_kx_2)))));
+		int Nx = 12; PID para ejes cambiados*/
+		Kcx = 1.5;
+		Tix = 0;
+		Tdx = 3;
+		int Nx = 20;
+		action[1] = (Kcx*e_kx + ((Tdx*Nx)/(1+(Nx*Ts/(e_kx_1 - e_kx_2)))));
 		// Anulamos "action[]" de este eje para medir sólo el otro.
 
 		// Control PID Law for Y-ROLL
 		// Roll Params
-		Kcy = 1.5;
+		/*Kcy = 1.5;
 		Tiy = 0;
 		Tdy = 3;
-		int Ny = 20;
-		action[1] = -(Kcy*e_ky + ((Tdy*Ny)/(1+(Ny*Ts/(e_ky_1 - e_ky_2)))));
+		int Ny = 20; PID para ejes cambiados*/
+		Kcy = 0.1;
+		Tiy = 0;
+		Tdy = 0.2;
+		int Ny = 16; //PID para final, a tirones
+		/*Kcy = 0.1;
+		Tiy = 0;
+		Tdy = 0.05;
+		int Ny = 12;*/
+		action[0] = -(Kcy*e_ky + ((Tdy*Ny)/(1+(Ny*Ts/(e_ky_1 - e_ky_2)))));
 
 		// Fin acciones de control
 
 
 		// Coulombic and Viscous for X axis only????????????????????????????????????????????????????????????????????????????
-		// double accion;
-		// accion = (abs(action[0]))+0.07;
-		// if (action[0] < 0)
-		//	accion = accion *(-1);
+		 double accion;
+		 accion = (abs(action[1]))+0.07;
+		 if (action[1] < 0)
+			accion = accion *(-1);
 
 		// Saturation Check
 		// FUNCION: action[0] = sat(action[0],0.6,-0.6);
 		double valor = action[0];
-		double top = 0.6;
-		double bottom = -0.6;
+		double top = 0.1; //0.6
+		double bottom = -0.1; //0.65
 
 		if (valor > top){
 			action[0] = top;
@@ -319,8 +339,8 @@ extern "C" {
 
 		// FUNCION action[1] = sat(action[1],0.6,-0.65);
 		valor = action[1];
-		top = 0.6;
-		bottom = -0.65;
+		top = 0.1; //0.6
+		bottom = -0.1; //-0.6
 
 		if (valor > top)
 		{
@@ -341,7 +361,7 @@ extern "C" {
 		// double dist = norm(d_wx,d_wy);
 		double dist = sqrt((d_wx*d_wx)+(d_wy*d_wy));
 
-		if ((dist < 0.5) && (i < numWaypoints-1))
+		if ((dist < 0.1) && (i < numWaypoints-1))
 			i = i+1;
 	}
 
